2425cd36d603f79f6c194de91ebc3fe20686b0d5
Andrey Sementsov <thesunlightday@gmail.com>
2024-12-16 15:44:08 +0400

    WIP Add auth

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/user', [AuthController::class, 'currentUser'])->middleware('auth:sanctum');
 
 Route::apiResource('/tasks', TaskController::class);
